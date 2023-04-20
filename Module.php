7fdba3730448f24eb49ca31b7a1e80b83b2cd71f
Vasil Sokolov <vasil@afterlogic.com>
2023-04-20 15:14:51 +0200

    Refactored getting module settings (part 3)

--- Module.php
+++ Module.php
@@ -19,7 +19,14 @@ namespace Aurora\Modules\SecuritySettingsWebclient;
 class Module extends \Aurora\System\Module\AbstractWebclientModule
 {
     /**
-     *
+     * @return Module
+     */
+    public static function getInstance()
+    {
+        return parent::getInstance();
+    }
+
+    /**
      * @return Module
      */
     public static function Decorator()
@@ -28,7 +35,6 @@ class Module extends \Aurora\System\Module\AbstractWebclientModule
     }
 
     /**
-     *
      * @return Settings
      */
     public function getModuleSettings()
